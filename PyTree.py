14418ac63dc32c54ea0c79b8e3cf9df99a7b2918
Manmeet Gill <contact@manmeetgill.com>
2016-10-15 19:01:41 +1100

    Improve variable names

--- PyTree.py
+++ PyTree.py
@@ -33,34 +33,34 @@ MAXDEPTH = 12  # maximum recursion depth
 LENGTHFACTOR = 120  # branch length factor
 
 # Create objects
-treefile = Image.new('RGB', (WIDTH, HEIGHT))
-draw = ImageDraw.Draw(treefile)
+TREEFILE = Image.new('RGB', (WIDTH, HEIGHT))
+DRAW = ImageDraw.Draw(TREEFILE)
 
 
-def tree(x1: int,  # The x co-ordinate for the start of the line
-         y1: int,  # The y co-ordinate for the start of the line
+def tree(xstart: int,  # The x co-ordinate for the start of the line
+         ystart: int,  # The y co-ordinate for the start of the line
          angle: int,  # the angle to branch out, in radians
          depth: int  # How far we have to go
          ) -> int:
     # If we haven't looped to the limit ...
     if depth > 0:
         # Get the end location of the lines
-        x2 = x1 + int(math.cos(math.radians(angle)) * depth * LENGTHFACTOR)
-        y2 = y1 + int(math.sin(math.radians(angle)) * depth * LENGTHFACTOR)
+        xend = xstart + int(math.cos(math.radians(angle)) * depth * LENGTHFACTOR)
+        yend = ystart + int(math.sin(math.radians(angle)) * depth * LENGTHFACTOR)
         # Get decimals for to draw based on how far in we are
-        (r, g, b) = colorsys.hsv_to_rgb(float(depth) / MAXDEPTH, 1.0, 1.0)
+        (reddec, greendec, bluedec) = colorsys.hsv_to_rgb(float(depth) / MAXDEPTH, 1.0, 1.0)
         # Turn them into RGB to draw
         # Yes, I know that rgb is 256 for red.
         # Red makes people angry so I want less of that.
-        red, green, blue = int(200 * r), int(255 * g), int(255 * b)
+        redhex, greenhex, bluehex = int(200 * reddec), int(255 * greendec), int(255 * bluedec)
         # Actually draw
-        draw.line([x1, y1, x2, y2], (red, green, blue), depth)
+        DRAW.line([xstart, ystart, xend, yend], (redhex, greenhex, bluehex), depth)
         # Draw the two that branch out from the end of that line.
-        tree(x2, y2, angle - SPREAD, depth - 1)
-        tree(x2, y2, angle + SPREAD, depth - 1)
+        tree(xend, yend, angle - SPREAD, depth - 1)
+        tree(xend, yend, angle + SPREAD, depth - 1)
 
 
 # Start drawing!
 tree(WIDTH / 2, HEIGHT, -90, MAXDEPTH)
-treefile = treefile.resize((1000, 1000), Image.ANTIALIAS)
-treefile.save("PyTree.png", "PNG")
+TREEFILE = TREEFILE.resize((1000, 1000), Image.ANTIALIAS)
+TREEFILE.save("PyTree.png", "PNG")
