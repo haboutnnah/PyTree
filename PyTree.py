d59b7d2db162c1b6b85b4195cd1f3857b80e9a87
Manmeet Gill <contact@manmeetgill.com>
2016-10-15 18:26:37 +1100

    Fix PEP8 error

--- PyTree.py
+++ PyTree.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw
 # Set variables
 # TODO: make these set by command line arguments
 SPREAD = 12  # how much branches spread apart
-AAFACTOR = 10 # How much bigger we render for anti-aliasing
+AAFACTOR = 10  # How much bigger we render for anti-aliasing
 FINALHEIGHT = 1000  # the height for the final image
 FINALWIDTH = 1000   # the width for the final image
 WIDTH = FINALWIDTH * AAFACTOR  # Rendered size
@@ -32,7 +32,9 @@ def tree(x1: int,  # The x co-ordinate for the start of the line
         y2 = y1 + int(math.sin(math.radians(angle)) * depth * LENGTHFACTOR)
         # Get decimals for to draw based on how far in we are
         (r, g, b) = colorsys.hsv_to_rgb(float(depth) / MAXDEPTH, 1.0, 1.0)
-        # Turn them into rgb to draw (using 200 for red, because red makes people angry
+        # Turn them into RGB to draw
+            # Yes, I know that rgb is 256 for red.
+            # Red makes people angry so I want less of that.
         red, green, blue = int(200 * r), int(255 * g), int(255 * b)
         # Actually draw
         draw.line([x1, y1, x2, y2], (red, green, blue), depth)
