39a08336858e5ba7def1d5b7a406deda35c2997c
Manmeet Gill <contact@manmeetgill.com>
2016-10-18 17:01:58 +1100

    Add command line arguments

--- PyTree.py
+++ PyTree.py
@@ -9,6 +9,7 @@ https://github.com/tf2manu994/PyTree
 """
 import colorsys
 import math
+import sys
 from PIL import Image, ImageDraw
 __author__ = "Manmeet Gill"
 __contact__ = "contact@manmeetgill.com"
@@ -17,8 +18,7 @@ __script__ = "PyHDDKeepAlive"
 __version__ = "v0.1"
 __git__ = "https://github.com/tf2manu994/PyTree"
 
-# Set variables
-# TODO: make these set by command line arguments
+# Set defaults
 SPREAD = 12  # how much branches spread apart
 AAFACTOR = 10  # How much bigger we render for anti-aliasing
 FINALHEIGHT = 1000  # the height for the final image
@@ -29,6 +29,53 @@ FINALHEIGHT = 1000  # the height for the final image
 FINALWIDTH = 1000   # the width for the final image
 MAXDEPTH = 12  # maximum recursion depth
 LENGTHFACTOR = 120  # branch length factor
+VALIDARGS = False
+
+for argument in sys.argv[1:]:
+    # Remove flag markers, we don't need to process those.
+    argument = argument.replace("-", "")
+    argument = argument.replace("/", "")
+    if argument[:6].lower() == "spread":
+        SPREAD = int(argument[7:])
+        print("Spread set to %s" % SPREAD)
+        VALIDARGS = True
+    elif argument[:8].lower() == "aafactor":
+        AAFACTOR = int(argument[9:])
+        print("AA Factor set to %s" % AAFACTOR)
+        VALIDARGS = True
+    elif argument[:6].lower() == "height":
+        FINALHEIGHT = int(argument[7:])
+        print("Height set to %s" % HEIGHT)
+        VALIDARGS = True
+    elif argument[:5].lower() == "width":
+        FINALWIDTH = int(argument[6:])
+        print("Width set to %s" % WIDTH)
+        VALIDARGS = True
+    elif argument[:5].lower() == "depth":
+        MAXDEPTH = int(argument[6:])
+        print("Depth set to %s" % MAXDEPTH)
+        VALIDARGS = True
+    elif argument[:6].lower() == "length":
+        LENGTHFACTOR = int(argument[7:])
+        print("Length  set to %s" % LENGTHFACTOR)
+        VALIDARGS = True
+    elif argument.lower() == help:
+        print("Valid Flags - Description - Default")
+        print("spread - How far the branches spread apart - 12")
+        print("aafactor - The factor at which to render the file - 10")
+        print("height - The height of the final file - 1000")
+        print("width - The width of the final file - 1000")
+        print("depth - How many layers to make the tree - 12")
+        print("length - The length of the branches - 120")
+    elif argument.lower() == "contact" or "copyright" or "author" or "version":
+        print(__author__)
+        print(__contact__)
+        print(__website__)
+        print(__script__, __version__)
+        print(__git__)
+        print("For help, run with the argument \"help\"")
+    else:
+        print("No arguments were given, running with defaults")
 
 # Create objects
 TREEFILE = Image.new('RGB', (WIDTH, HEIGHT))
